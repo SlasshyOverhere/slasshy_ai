8967b533c5eb62982fc944663902dd7c0d3ac580
unknown <5boned00@gmail.com>
2023-02-22 12:25:40 +0000

    update the server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     // Slasshy-----!!!!
 
-     const response = await fetch('http://localhost:5000', {
+     const response = await fetch('https://slasshy-ai.onrender.com', {
          method: 'POST',
         headers: {
             'Content-Type': 'application/json',
