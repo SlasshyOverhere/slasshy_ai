8c8f06bf9ba8431f9910279e762e87d5c6808b46
unknown <5boned00@gmail.com>
2023-02-22 13:50:44 +0000

    Final

--- client/script.js
+++ client/script.js
@@ -86,32 +86,30 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    // Slasshy-----!!!!
-
-     const response = await fetch('https://slasshy-ai.onrender.com', {
-         method: 'POST',
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-       },
-         body: JSON.stringify({
-             prompt: data.get('prompt')
-         })
-     })
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
 
-     clearInterval(loadInterval)
-     messageDiv.innerHTML = " "
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
 
-     if (response.ok) {
-         const data = await response.json();
-         const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-         typeText(messageDiv, parsedData)
-     } else {
-         const err = await response.text()
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong. Please try again later. Contact @notslasshy."
+        messageDiv.innerHTML = "Something went wrong"
         alert(err)
-     }
+    }
 }
 
 form.addEventListener('submit', handleSubmit)
